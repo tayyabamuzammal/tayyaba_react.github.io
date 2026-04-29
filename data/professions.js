0e9e03343916f09e5edc3a82d05b8fd3ea212fff
tayyabamuzammal <tayyabamuzammal24@gmail.com>
2026-04-29 22:41:42 +0500

    Create professions.js

---
+++ data/professions.js
@@ -0,0 +1,5 @@
+export const professions = [
+  "Doctor", "Engineer", "Teacher", "Designer", "Developer",
+  "Artist", "Photographer", "Writer", "Nurse", "Lawyer",
+  "Chef", "Pilot", "Manager", "Scientist", "Architect"
+];
