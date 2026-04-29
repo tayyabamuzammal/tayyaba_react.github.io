578a769df255417f79bd3e68d2f79a0ee9172bd7
tayyabamuzammal <tayyabamuzammal24@gmail.com>
2026-04-29 22:40:33 +0500

    Create profiles.js

---
+++ data/profiles.js
@@ -0,0 +1,26 @@
+export const profiles = [
+  {
+    id: 1,
+    name: "Ayesha",
+    age: 20,
+    info: "Student",
+    img: "https://i.pravatar.cc/150?img=1",
+    bg: "#fff0f5",
+  },
+  {
+    id: 2,
+    name: "Fatima",
+    age: 21,
+    info: "Designer",
+    img: "https://i.pravatar.cc/150?img=2",
+    bg: "#f0fff0",
+  },
+  {
+    id: 3,
+    name: "Zainab",
+    age: 22,
+    info: "Developer",
+    img: "https://i.pravatar.cc/150?img=3",
+    bg: "#f0f8ff",
+  },
+];
